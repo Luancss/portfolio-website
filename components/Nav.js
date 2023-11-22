59272ee3b95bb07dd043dfa5b60784e532651d54
Luancss <ventobo123@gmail.com>
2023-11-22 15:24:30 -0300

    Navigation

--- components/Nav.js
+++ components/Nav.js
@@ -6,40 +6,64 @@ import {
   HiRectangleGroup,
   HiChatBubbleBottomCenterText,
   HiEnvelope,
-} from 'react-icons/hi2';
+} from "react-icons/hi2";
 
 // nav data
 export const navData = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
   {
-    name: 'testimonials',
-    path: '/testimonials',
+    name: "testimonials",
+    path: "/testimonials",
     icon: <HiChatBubbleBottomCenterText />,
   },
   {
-    name: 'contact',
-    path: '/contact',
+    name: "contact",
+    path: "/contact",
     icon: <HiEnvelope />,
   },
 ];
 
 // next link
-import Link from "next/link"
+import Link from "next/link";
 
 // next router
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
 
 const Nav = () => {
-  return <nav>
-    <div>
-      {navData.map((link, index) => {
-        return <Link href={link.path}>{link.icon}</Link>
-      })}
-    </div>
-  </nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+  return (
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
+      {/* inner */}
+      <div className="flex w-full xl:flex-col items-center justify-between xl:justfy-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80] xl:h-max py-8 bg-white/10 backdrop-blue-sm text-3xl xl:text-xl xl:rounded-full">
+        {navData.map((link, index) => {
+          return (
+            <Link
+              className={`${link.path === pathname && "text-accent"
+                } relative flex items-center group hover:text-accent transition-all duration-300`}
+              href={link.path}
+              key={index}
+            >
+              {/* tooltip */}
+              <div className="absolute pr-14 right-0 hidden xl:group-hover:flex">
+                <div className="bg-white relative flex text-primary items-center p-[6px] rounded-[3px]">
+                  <div className="text-[12px] leading-none font-semibold capitalize">{link.name}
+                  </div>
+                  {/* triangle */}
+                  <div className="border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2"></div>
+                </div>
+              </div>
+              {/* icon */}
+              <div>{link.icon}</div>
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
