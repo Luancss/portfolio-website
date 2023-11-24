b0756418eb56653c1a162fbf273f9aec226a7b04
Luancss <ventobo123@gmail.com>
2023-11-24 02:27:54 -0300

    Header complete

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,34 @@
+// next image
+import Image from "next/image";
+
+// next link
+import Link from "next/link";
+
+// components
+import Socials from "../components/Socials";
+
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+          {/* logo */}
+          <Link href={"/"}>
+            {/* <h1 className="text-4xl flex items-center" priority={true}><b className="px-2">Luan</b><p className="text-accent">Carlos</p></h1> */}
+            <Image
+              src={"/logo.svg"}
+              width={220}
+              height={48}
+              alt=""
+              priority={true}
+            />
+          </Link>
+          {/* socials */}
+          <Socials />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,43 @@
+// links
+import Link from "next/link";
+
+// icons
+import {
+  RiInstagramLine,
+  RiGithubFill,
+  RiMailFill,
+  RiLinkedinFill,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link
+        href={"https://github.com/Luancss"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiGithubFill />
+      </Link>
+      <Link
+        href={"https://www.linkedin.com/in/luan-carlos-30035b246/"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiLinkedinFill />
+      </Link>
+      <Link
+        href={"https://www.instagram.com/luanworth"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiInstagramLine />
+      </Link>
+      <Link
+        href="mailto:luancss.contact@gmail.com"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiMailFill />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
