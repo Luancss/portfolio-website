cb80320a7fda57861fa69da1b6ffe9e3440dca6a
Luancss <ventobo123@gmail.com>
2023-11-24 01:30:39 -0300

    Particles container complete

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ const Home = () => {
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0">
         </div>
         {/* particles */}
-        <div></div>
+        <ParticlesContainer/>
         {/* avatar img */}
         <motion.div
           variants={fadeIn("up", 0.2)}
